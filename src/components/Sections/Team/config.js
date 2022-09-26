a0b71b3dc24ff8a1ab855afe544e70d4f6bc44ae
Carlo <carlo.buckets@gmail.com>
2022-09-26 14:04:40 +0800

    Updates.

---
+++ src/components/Sections/Team/config.js
@@ -0,0 +1,26 @@
+const Info = [
+  {
+    name: "Francis Manaig",
+    title: "Software Engineer",
+    pic: "/assets/cis.png"
+  },
+  {
+    name: "Jonas Ivy Imperial",
+    title: "Software Engineer",
+    pic: "/assets/junasu.jpeg"
+  },
+  {
+    name: "Rommel Paa",
+    title: "Software Engineer",
+    pic: "/assets/mel.jpeg"
+  },
+  {
+    name: "Carlo Carpio",
+    title: "Software Engineer",
+    pic: "/assets/carlo.jpeg"
+  },
+]
+
+export {
+  Info
+}
---
+++ src/components/Sections/Team/index.svelte
@@ -0,0 +1,22 @@
+<script>
+	import Title from '../../Title.svelte';
+  import { Info } from './config'
+</script>
+ 
+<section class="relative pb-[80px] md:pt-[80px]" id="services">
+  <div class="flex justify-center flex-col">
+    <div class="pt-[120px] pb-[30px]">
+      <Title title="our" subtitle="team" />
+    </div>
+
+    <div class="w-full flex flex-col flex-wrap md:flex-row lg:px-[120px] mx-0 my-auto">
+      {#each Info as { name, title, pic }}
+        <div class="p-[30px] flex-1 text-center hover:bg-boxDarkHover md:min-w-[50%] lg:min-w-[25%]">
+          <img class="max-h-[150px] m-auto rounded" src={pic} alt={name} />
+          <h4 class="text-light font-primary text-[34px] inline border-b-4 border-solid border-primary uppercase">{name}</h4>
+          <p class="text-light text-[16px] leading-[26px] font-[100] pt-[20px] m-0">{title}</p>
+        </div>
+      {/each}
+    </div>
+  </div>
+</section>
